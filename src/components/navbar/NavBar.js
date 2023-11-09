da6f17dfe85ed5bfcaafabe0efa09cc470800413
Monica Olivares <mona.ov89@gmail.com>
2023-11-09 11:52:22 -0800

    nov9 5

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -7,11 +7,15 @@ import 'bulma/css/bulma.min.css';
 const NavBar = () =>{
     return ( 
     <nav className="has-background-light">
-        <div>
-       <figure >
-       <img src={BoticaLogo}></img>
-       </figure>
-        <h2 class="is-size-4 has-text-centered has-text-weight-bold is-family-monospace">Botica Bosque</h2>
+        <div className="columns is-multiline is-mobile is-centeredn is-vcentered">
+                <div className="column is-narrow">
+                    <img src={BoticaLogo}></img>
+                    
+                </div>
+                <div className="column is-8 ">
+                    <h2 class="is-size-4 has-text-centered has-text-weight-bold is-family-monospace">Botica Bosque</h2>
+                </div>
+        
         </div>
 
         <div className="columns">
@@ -25,7 +29,9 @@ const NavBar = () =>{
             </div>
          
             <div className="column">
+                
                 <button className="column button is-primary is-light"><a src="">Quienes somos</a></button>
+                
             </div>
             <div className="column">
                 <button className="button is-primary is-light"><a src="">Talleres y actividades</a></button>
