192fd3852241a46d4a95f142ef0690358448fe29
Monica Olivares <mona.ov89@gmail.com>
2023-11-09 09:40:03 -0800

    nov9 1

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -6,20 +6,26 @@ import CartWidget from "../CartWidget/CartWidget";
 import 'bulma/css/bulma.min.css'; 
 const NavBar = () =>{
     return ( 
-    <nav>
-        <div className="columns is-centered">
-        <img src={BoticaLogo} className="column is-half"></img>
-        <h2 class="is-half">Botica Bosque</h2>
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+        <a src=""><img src={BoticaLogo}></img></a>
+        <h2 class="is-size-4 has-text-centered has-text-weight-bold is-family-monospace">Botica Bosque</h2>
+        
 
         
         </div>
-        <div className="hero">
-        <div clasName="hero-body">
-            <button>Cosmetica natural</button>
-            <button>Preparados herbales</button>
-            <button>Quienes somos</button>
-            <button>Talleres y actividades</button>
-            <button>E-books</button>
+        <div>
+        <div clasName="navbar-brand">
+            <div className="navbar-item has-dropdown is-hoverable"> 
+                <button className="navbar-item"><a>Cosmetica natural</a></button>
+                <button className="navbar-item"><a>Preparados herbales</a></button>
+            </div>
+            <hr class="navbar-divider"></hr>
+            <button className="navbar-item"><a>Quienes somos</a></button>
+            <hr class="navbar-divider"></hr>
+            <button className="navbar-item"><a>Talleres y actividades</a></button>
+            <hr class="navbar-divider"></hr>
+            <button className="navbar-item"><a>E-books</a></button>
 
             <CartWidget></CartWidget>
 
